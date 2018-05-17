4f6e55af49a186ac90e44a2852ecb3991edaf799
pasasoft_mini <pasasoft_mini@pasasofnideMini.lan>
2018-05-17 17:57:45 +0800

    first time to submit project

---
+++ Zhangyongjie.CustomKit/Zhangyongjie.CustomKit/CustomTabbedPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Zhangyongjie.CustomKit
+{
+    public partial class CustomTabbedPage : ContentPage
+    {
+        public CustomTabbedPage()
+        {
+            InitializeComponent();
+        }
+
+        //public void addChildren(ContentPage page, string title = "",Image image)
+        //{
+        //    this.Children.Add
+        //}
+
+    }
+}
---
+++ Zhangyongjie.CustomKit/Zhangyongjie.CustomKit/TestTablePage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Zhangyongjie.CustomKit
+{
+    public class TestTablePage : ContentPage
+    {
+        public TestTablePage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
